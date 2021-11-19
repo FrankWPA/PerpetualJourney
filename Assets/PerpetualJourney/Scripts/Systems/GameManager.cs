69cafaa01a95b6456f5b066b2188c258a90cb21d
Frank Wallace Potratz de Andrade <frankwallace@edu.univali.br>
2021-11-19 17:38:52 -0400

    scores and save funtionality

--- Assets/PerpetualJourney/Scripts/Systems/GameManager.cs
+++ Assets/PerpetualJourney/Scripts/Systems/GameManager.cs
@@ -1,8 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using System.IO;
+using System.Net.Sockets;
 using UnityEngine;
-using UnityEngine.InputSystem;
-using UnityEngine.SceneManagement;
 using TMPro;
 
 namespace PerpetualJourney
@@ -11,9 +10,14 @@ namespace PerpetualJourney
     {
         [SerializeField]private Player _player;
         [SerializeField]private GameEvents _gameEvents;
-        [SerializeField]private TextMeshProUGUI _scoreText;
         [SerializeField]private GameObject _gameOverUi;
+        [SerializeField]private TextMeshProUGUI _scoreText;
+        [SerializeField]private TextMeshProUGUI _gameOverScore;
+        [SerializeField]private GameObject _highscoreIndicator;
         
+        private ScoreObject _scoreObject = new ScoreObject();
+
+        private const string DefaultNewScore = "New!";
 
         public void Initialize()
         {
@@ -28,6 +32,7 @@ namespace PerpetualJourney
             _player.SceneReset();
             _scoreText.gameObject.SetActive(true);
             _gameOverUi.gameObject.SetActive(false);
+            _highscoreIndicator.SetActive(false);
         }
 
         private void OnDisable()
@@ -44,6 +49,7 @@ namespace PerpetualJourney
 
         private void UpdateTextScore(int score)
         {
+            _scoreObject.value = score;
             _scoreText.SetText("Score: {0}", score);
         }
 
@@ -51,6 +57,55 @@ namespace PerpetualJourney
         {
             _scoreText.gameObject.SetActive(false);
             _gameOverUi.gameObject.SetActive(true);
+
+            DisplayScores();
+        }
+
+        private void DisplayScores()
+        {
+            ScoreObject highscore = GetHighScore();
+            string firstLine = _scoreObject.value.ToString();
+            string lastLine = DefaultNewScore;
+            
+            if (highscore.value != 0)
+            {
+                lastLine = highscore.value.ToString();
+            }
+
+            if(_scoreObject.value > highscore.value)
+            {
+                SaveHighScore();
+                _highscoreIndicator.SetActive(true);
+                firstLine = "<color=green>" + firstLine + "</color>";
+            }
+            
+            _gameOverScore.SetText(firstLine + "\n" + lastLine);
+        }
+
+        private ScoreObject GetHighScore()
+        {
+            string scorePath = Application.dataPath + "/highscore.txt";
+
+            if(File.Exists(scorePath))
+            {
+                string json = File.ReadAllText(scorePath);
+                ScoreObject scoreObject = JsonUtility.FromJson<ScoreObject>(json);
+                return scoreObject;
+            }
+
+            return new ScoreObject();
+        }
+
+        private void SaveHighScore()
+        {
+            string json = JsonUtility.ToJson(_scoreObject);
+            File.WriteAllText(Application.dataPath + "/highscore.txt", json);
         }
     }
+    
+    class ScoreObject
+    {
+        public int value = 0;
+    }
 }
+
